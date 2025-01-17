83a07271540e038d9e32510896a1b00603ab0cbb
Fisielu <kubam1996@gmail.com>
2025-01-17 01:08:28 +0100

    Final

--- src/main/java/com/example/firstrestapi/product/support/exeption/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstrestapi.product.support.exeption;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long id) {
-        super(String.format("Product with %d not found", id));
-    }
-}
